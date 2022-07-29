4c9a6c22aa28611793e2c602ebbe6ff203cc3a8c
DongHwan <kdhpopoya@gmail.com>
2022-07-29 14:33:24 +0900

    feat: @yearnlune/mongodb-search-interface 추가
    - @yearnlune/mongodb-search-interface 패키지 배포
    - NPM 배포 CI 추가

---
+++ mongodb-search-interface/node/generator.js
@@ -0,0 +1,41 @@
+const {GraphQLDefinitionsFactory} = require('@nestjs/graphql');
+const fs = require('fs');
+const path = require('path');
+
+const graphqlResourceDirectory = path.join(process.cwd(), '../build/resources/main/graphql')
+const autoGenerateTypescriptFile = path.join(process.cwd(), 'src/graphql.ts');
+const outputGraphqlDirectory = path.join(process.cwd(), 'graphql')
+
+copyRecursiveSync(graphqlResourceDirectory, outputGraphqlDirectory)
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+
+definitionsFactory.generate({
+  typePaths: [`${graphqlResourceDirectory}/*.graphqls`],
+  path: autoGenerateTypescriptFile,
+  outputAs: 'interface',
+  emitTypenameField: true,
+  customScalarTypeMapping: {
+    LONG: 'number',
+    JSON: 'any'
+  },
+  federation: false,
+  enumsAsTypes: true
+});
+
+function copyRecursiveSync(src, dest) {
+  const exists = fs.existsSync(src);
+  const stats = exists && fs.statSync(src);
+  const isDirectory = exists && stats.isDirectory();
+  if (isDirectory) {
+    if (!fs.existsSync(dest) || !fs.statSync(dest).isDirectory()) {
+      fs.mkdirSync(dest)
+    }
+    fs.readdirSync(src).forEach(function (childItemName) {
+      copyRecursiveSync(path.join(src, childItemName),
+              path.join(dest, childItemName));
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
---
+++ mongodb-search-interface/node/src/index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import * as MongodbSearchGQL from "./graphql"
+
+export {
+  MongodbSearchGQL
+}
